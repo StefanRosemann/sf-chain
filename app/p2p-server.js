4f1a27710ca8b1737933004bf9bf13b1eaf32de2
Stefan Rosemann <info@stefan-rosemann.de>
2024-02-15 19:15:38 +0100

    Ch6 finished

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -30,14 +30,25 @@ class P2pServer {
 
       this.messageHandler(socket);
       
-      socket.send(JSON.stringify(this.blockchain.chain));
-    }
+      this.sendChain(socket);
+   }
    
-   messageHandler(socket) {
+    messageHandler(socket) {
       socket.on('message', message => {
-       const data = JSON.parse(message);
-       console.log('data', data);
-     });
+         const data = JSON.parse(message);
+         
+         this.blockchain.replaceChain(data);
+      });
+   }
+
+   sendChain(socket) {
+      socket.send(JSON.stringify(this.blockchain.chain));
+   }
+
+   syncChains() {
+      this.sockets.forEach(socket => {
+         this.sockets.forEach(socket => this.sendChain(socket));
+      });
    }
 }
 
