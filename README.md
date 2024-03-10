b8dd9c8f42624e8418800e316780d34f67fb9462
Stefan Rosemann <info@stefan-rosemann.de>
2024-03-10 21:42:38 +0100

    ch9 finished

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # sf-chain
+
+# start-cmd:
+npm run dev
+HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
+HTTP_PORT=3003 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
+
+# REST-API
+GET: localhost:3001/blocks
+
+POST: localhost:3001/mine
+{
+"data": "bar"
+}
+
+GET:  localhost:3001/transactions
+
+POST: localhost:3001/transact
+{
+	"recipient": "foo-4dr3ss",
+	"amount": 50
+}
+
+GET: localhost:3001/public-key
+
+
+
--- app/index.js
+++ app/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
@@ -23,6 +26,21 @@ app.post('/mine', (req, res) => {
   p2pServer.syncChains();
   
   res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  p2pServer.broadcastTransaction(transaction);
+  res.redirect('/transactions');
+});
+
+app.get('/public-key', (req, res) => {
+  res.json({ publicKey: wallet.publicKey });
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
